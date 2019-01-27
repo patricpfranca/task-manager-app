c99fd23fb2f62e4864550d9b9357e3cc76cbf200
Patric Pereira <patric.pereirafranca@gmail.com>
2019-01-27 12:13:03 -0200

    adding ns-angula2-token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+import { NSAngular2TokenService } from "./shared/ns-angular2-token/ns-angular2-token.service";
 
 @Component({
-    selector: "ns-app",
-    moduleId: module.id,
-    templateUrl: "./app.component.html",
+  selector: "ns-app",
+  moduleId: module.id,
+  templateUrl: "./app.component.html",
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    private tokenService: NSAngular2TokenService
+  ) {
+    this.tokenService.init({
+      apiBase: 'apiBase',
+      globalOptions: {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/vnd.taskmanager.v2'
+        }
+      }
+    })
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from "./sign-up/sign-up.component";
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NSAngular2TokenService } from './shared/ns-angular2-token/ns-angular2-token.service';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -14,22 +15,24 @@ import { SignUpComponent } from "./sign-up/sign-up.component";
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule,
-        AppRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        SignInComponent,
-        SignUpComponent
-    ],
-    providers: [],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  bootstrap: [
+    AppComponent
+  ],
+  imports: [
+    NativeScriptModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  providers: [
+    NSAngular2TokenService
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ]
 })
 /*
 Pass your application module to the bootstrapModule function located in main.ts to start your app
