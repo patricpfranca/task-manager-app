0d4d1e7ecd56e17b33990faeb1a9f42200b9a61d
Patric Pereira <patric.pereirafranca@gmail.com>
2019-01-27 14:24:24 -0200

    fixing indentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { Routes } from "@angular/router";
 
 import { SignInComponent } from "./sign-in/sign-in.component";
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from "./sign-up/sign-up.component";
 
 const routes: Routes = [
-    { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-    { path: 'sign-in', component: SignInComponent },
-    { path: 'sign-up', component: SignUpComponent }
+  { path: "", redirectTo: "/sign-in", pathMatch: "full" },
+  { path: "sign-in", component: SignInComponent },
+  { path: "sign-up", component: SignUpComponent }
 ];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
+  imports: [NativeScriptRouterModule.forRoot(routes)],
+  exports: [NativeScriptRouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
