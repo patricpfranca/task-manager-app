0b3c995973b2f4b7b8fe558d264e432e4f261951
Patric Pereira <patric.pereirafranca@gmail.com>
2019-01-27 15:39:29 -0200

    adding Home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Routes } from '@angular/router';
 
-import { SignInComponent } from "./sign-in/sign-in.component";
-import { SignUpComponent } from "./sign-up/sign-up.component";
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/sign-in", pathMatch: "full" },
-  { path: "sign-in", component: SignInComponent },
-  { path: "sign-up", component: SignUpComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
@@ -28,6 +29,7 @@ import { TokenService } from './shared/token.service';
   ],
   declarations: [
     AppComponent,
+    HomeComponent,
     SignInComponent,
     SignUpComponent
   ],
