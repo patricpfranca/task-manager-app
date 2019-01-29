1bc4c24b866e41b65420a04dfd238c4dc0f7f6f8
Patric Pereira <patric.pereirafranca@gmail.com>
2019-01-29 21:06:44 -0200

    implementing signOutUser

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ns-home',
   moduleId: module.id,
   templateUrl: './home.component.html'
 })
-export class HomeComponent { }
+export class HomeComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  public signOutUser() {
+    this.authService.signOut()
+      .subscribe(
+        () => this.router.navigate(['/sign-in'])
+      );
+  }
+}
